f822407895660f4fd0c678e803367e91dfe12950
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2023-09-05 14:26:27 +0530

    Change model

--- download.py
+++ download.py
@@ -6,11 +6,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     tokenizer = AutoTokenizer.from_pretrained(
-        "codellama/CodeLlama-34b-Instruct-hf",
+        "TheBloke/CodeLlama-34B-GPTQ",
         use_cache="cache"
     )
     model = AutoModelForCausalLM.from_pretrained(
-        "codellama/CodeLlama-34b-Instruct-hf",
+        "TheBloke/CodeLlama-34B-GPTQ",
         torch_dtype=torch.bfloat16,
         low_cpu_mem_usage=True,
         use_cache="cache"
